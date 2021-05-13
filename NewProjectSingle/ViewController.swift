9863d3f05e75e015734fce51c8bc708c3c6f817d
SatendraSattu <satendrasinghmca@gmail.com>
2021-05-13 21:10:14 +0530

    Initial Commit

---
+++ NewProjectSingle/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewProjectSingle
+//
+//  Created by Satendra Pal Singh on 23/02/1943 Saka.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
